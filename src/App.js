546b9b15e4c1fda8fb4959df8e886b6cef35d901
jjgonz987 <jhovany990@gmail.com>
2019-02-23 01:58:00 -0600

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'reactstrap';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import Navigation from './Components/Navigation/Navigation'
+
+const landing = () => {
+  return (
+    <div>
+      <p>Fuck you facebook</p>
+      <Button color="primary">primary</Button>{' '}
+    </div>
+  )
+}
+
+const landing1 = () => {
+  return (
+    <div>
+      <p>Fuck you facebfadfadfasfasfook</p>
+      <Button color="primary">primary</Button>{' '}
+    </div>
+  )
+}
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navigation />
+        <BrowserRouter>
+        <Switch>
+          <Route path= "/" component={landing} exact/>
+          <Route component={() => "404 not found :("}/>
+        </Switch>
+      </BrowserRouter>
       </div>
     );
   }
